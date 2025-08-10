2bebe7ee4b566ab8862b005051427a9d2c04567f
accio-uday <udaymanideep2002@gmail.com>
2025-08-10 20:38:00 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,13 @@
 
 import React from "react";
-import './../styles/App.css';
+import ButtonCounter from './ButtonCounter';
 
-const App = () => {
+function App() {
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div className="App">
+    <ButtonCounter />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
